827801f7a940089882cffbd490ec1cc849a41bff
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2024-12-22 12:04:00 -0500

    Final main.py commit

--- main.py
+++ main.py
@@ -32,25 +32,58 @@ def banking_choice():
 
 #Define all banking options
 def SHOW_BALANCE():
-      print(f"Your balance is currently {bank_balance}. ")
+      print(f"Your balance is currently ${bank_balance} ")
       another_option()
 
+def WITHDRAWAL():
+      global bank_balance
+      print(f"Your balance is currently ${bank_balance} ")
+      with_amount = float(input("How much would you like to withdraw?: "))
+      bank_balance -= with_amount
+      print(f"After withdrawal, your balance is ${bank_balance} ")
+      another_option()
 
+def DEPOSIT():
+      global bank_balance
+      print(f"Your balance is currently ${bank_balance} ")
+      dep_amount = float(input("How much would you like to deposit?: "))
+      bank_balance += dep_amount
+      print(f"After your deposit, your balance is ${bank_balance} ")
+      another_option()
+
+def EXIT():
+      print("Thanks for banking with us today!")
+
+#Creating a function that easily loops the banking program
 def another_option():
       option = input("Would you like to choose another option? (YES/NO): ")
-      if option.lower() == "'yes":
+      if option.lower() == "yes":
             start_menu()
-            banking_choice()
-      elif option.lower == "no":
+            choice = banking_choice()
+            handle_choice(choice)
+      elif option.lower() == "no":
             print("Thank you for banking with us today! ")
       else:
             print("That is not a valid option")
+            another_option()
 
+#Creating function to handle banking choice
+def handle_choice(choice):
+      if choice == 1:
+            SHOW_BALANCE()
+      elif choice == 2:
+            WITHDRAWAL()
+      elif choice == 3:
+            DEPOSIT()
+      elif choice == 4:
+            EXIT()
+            return
+
+#Defining main program
 def main():
       start_menu()
-      banking_choice()
-      if banking_choice() == 1:
-            SHOW_BALANCE()
+      choice = banking_choice()
+      handle_choice(choice)
 
 if __name__ == "__main__":
       main()
